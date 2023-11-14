39d0751801751285c0358c0cd1bac2361a2895a8
ProgrammSkill <antonkant2018@gmail.com>
2023-11-14 17:46:48 +0300

    Added a view class to view a specific executor

--- freelance_app/urls.py
+++ freelance_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('executors/<int:pk>', ExecutorRetrieveView.as_view(), name='executor'),
     path('tickets/all', TicketListView.as_view(), name='tickets_all'),
 ]
--- freelance_app/views.py
+++ freelance_app/views.py
@@ -4,6 +4,11 @@ from .models import *
 from .serializers import *
 
 
+class ExecutorRetrieveView(generics.RetrieveAPIView):
+    queryset = Executor.objects.all()
+    serializer_class = ExecutorSerializer
+
+
 class TicketListView(generics.ListAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
