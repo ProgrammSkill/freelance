0d94d40e7ce61754275f92c32bc1f15849644dae
ProgrammSkill <antonkant2018@gmail.com>
2023-11-15 10:19:16 +0300

    Added urls to endpoint executor

--- freelance_app/urls.py
+++ freelance_app/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('executors/<int:pk>', ExecutorRetrieveView.as_view(), name='executor'),
+    path('executors/update/<int:pk>', ExecutorUpdateView.as_view(), name='executor_update'),
+    path('executors/all', ExecutorListView.as_view(), name='executors_all'),
+    path('executor/new', ExecutorCreateView.as_view(),name='executor_new'),
     path('tickets/all', TicketListView.as_view(), name='tickets_all'),
 ]
