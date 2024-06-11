50810a6d4ed9404fb4daadaac751cbc4747c5460
ProgrammSkill <antonkant2018@gmail.com>
2024-06-11 16:55:25 +0300

    Fix key

--- freelance/settings.py
+++ freelance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c-a3ru=z)+p61k71ar2(mnagd)qgq!gi)ebi+0jb7!0m0g!e1%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
