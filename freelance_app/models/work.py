1e8c02a23dec96cb95b24c912c63bd1be1667712
ProgrammSkill <antonkant2018@gmail.com>
2024-06-25 15:31:52 +0300

    Refactoring the Service and Service models

--- freelance_app/models/work.py
+++ freelance_app/models/work.py
@@ -1,11 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin, Group, Permission
-from django.contrib.auth.models import AbstractBaseUser
-from django.utils import timezone
 from .user import User
 
 
-class Service(models.Model):
+class BaseService(models.Model):
     SERVICE_TYPES = [
         ('1', 'Веб разработка'),
         ('2', 'Маркетинг'),
@@ -16,39 +13,32 @@ class Service(models.Model):
         ('7', 'Фотография'),
     ]
 
-    executor = models.ForeignKey('User', on_delete=models.CASCADE)
+    service_type = models.CharField(choices=SERVICE_TYPES, default='1', max_length=1)
     name = models.CharField(max_length=250)
     desk = models.CharField(max_length=1000)
     price = models.IntegerField()
-    service_type = models.CharField(choices=SERVICE_TYPES, default='1', max_length=1)
 
     def __str__(self):
-        return f"{self.name}, {self.get_service_type_display()}, price: {self.price}"
+        return f"{self.name}, price: {self.price}"
+
+    class Meta:
+        abstract = True
+
+class Service(BaseService):
+    executor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.get_service_type_display()}"
 
     class Meta:
         verbose_name = 'Услуги'
         verbose_name_plural = 'Услуги'
 
-
-class Order(models.Model):
-    ORDER_TYPES = [
-        ('1', 'Веб разработка'),
-        ('2', 'Маркетинг'),
-        ('3', 'Копирайтинг'),
-        ('4', 'Рерайтинг'),
-        ('5', 'Переводы'),
-        ('6', 'Видеомонтаж'),
-        ('7', 'Фотография'),
-    ]
-
-    customer = models.ForeignKey('User', on_delete=models.CASCADE)
-    name = models.CharField(max_length=250)
-    desk = models.CharField(max_length=1000)
-    price = models.IntegerField()
-    order_type = models.CharField(choices=ORDER_TYPES, default='1', max_length=1)
+class Order(BaseService):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}, {self.get_order_type_display()}, price: {self.price}"
+        return f"{super().__str__()}, {self.get_order_type_display()}"
 
     class Meta:
         verbose_name = 'Заказы'
